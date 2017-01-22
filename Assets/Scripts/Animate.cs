e1ae4605735bab9a07ef7932977fa94724293241
JunglerBot <rmiguelcsfontes@gmail.com>
2017-01-22 12:59:01 +0000

    ultimas alterações partI
    -boeca
    -movimentos

---
+++ Assets/Scripts/Animate.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animate : MonoBehaviour {
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+
+        anim = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        /*
+         * if (Press left)
+         * {
+         *      anim.setBool("left", true);
+         * }
+         * else
+         * {
+         *      anim.setBool("left", false);
+         * }
+           
+            if (Press right)
+         * {
+         *      anim.setBool("right", true);
+         * }
+         * else
+         * {
+         *      anim.setBool("right", false);
+         * }
+
+
+        */
+    }
+}
---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public bool isPaused = false;
+
+	
+    public void PauseGame()
+    {
+        isPaused = true;
+        Time.timeScale = 0;
+    }
+}
