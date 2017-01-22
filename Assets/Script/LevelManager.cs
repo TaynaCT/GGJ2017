8ef28d7f61f34084cb408eb56d1c887c447696fb
JunglerBot <rmiguelcsfontes@gmail.com>
2017-01-22 14:41:58 +0000

    Mais coisas
    Coisas

--- Assets/Script/LevelManager.cs
+++ Assets/Script/LevelManager.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    public Transform mainMenu, settings, credits, howToPlay;
+    public Transform mainMenu, credits, howToPlay;
 
     //Escolher a cena a carregar
     public void LoadScene(string name)
@@ -13,19 +13,6 @@ public class LevelManager : MonoBehaviour
         SceneManager.LoadScene(name);
     }
 
-    public void SettingsMenu(bool clicked)
-    {
-        if(clicked==true)
-        {
-            settings.gameObject.SetActive(clicked);
-            mainMenu.gameObject.SetActive(false);
-        }
-        else
-        {
-            settings.gameObject.SetActive(clicked);
-            mainMenu.gameObject.SetActive(true);
-        }
-    }
 
     public void CreditsMenu(bool clicked)
     {
