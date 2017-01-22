7ac2e318fc5ad5532ca35adfd22ea7fda3116b2a
Tayná Tinôco <a10729@alunos.ipca.pt>
2017-01-22 02:18:47 +0000

    Ja gera ondas aleatorias
    as ondas andam ao ritmo da musica

--- Assets/Script/SetWaves.cs
+++ Assets/Script/SetWaves.cs
@@ -6,20 +6,46 @@ public class SetWaves : MonoBehaviour {
 
     public GameObject wavePrefab;
     GameObject[] waves = new GameObject[20];
+
+    float timer;
 	// Use this for initialization
 	void Start () {
-        for (int i = 0; i < 10; i++)
+        timer = 0;
+        for (int i = 0; i < 20; i++)
         {
             GameObject tempWave = (GameObject)Instantiate(wavePrefab);
+                          
             tempWave.transform.position = transform.position + new Vector3(i + 1.5f, 0, 0);
             tempWave.name = "Wave" + i;
-            tempWave.transform.position += new Vector3(i + 2f, 0, 0);
+            if (i > 10)
+                tempWave.transform.position -= new Vector3(i + 2f, 0, 0);
+            else
+                tempWave.transform.position += new Vector3(i + 2f, 0, 0);
+
             waves[i] = tempWave;
+            waves[i].active = false;            
+                 
         }
+       
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        int count = 0;
+        int randWave = Random.Range(0, 20);
+        int randFreq = Random.Range(0, 8);
+
+        while (count < 10)
+        {
+            waves[randWave].active = true;
+
+            if((waves[randWave] != null )&& (waves[randWave].active))
+                waves[randWave].transform.position -= new Vector3(0, 0, Spectrum.freqBand[randFreq] * .01f);
+
+            Debug.Log("randFreq =" + randFreq + "freqBand[randFreq] =" + Spectrum.freqBand[randFreq]);
+
+            count++;
+        }
+        
+    }
 }
--- Assets/Script/Spectrum.cs
+++ Assets/Script/Spectrum.cs
@@ -5,7 +5,7 @@ using System.Collections;
 public class Spectrum : MonoBehaviour
 {
     public static float[] samples = new float[512];
-    public static float[] freqBand = new float[8];
+    public static float[] freqBand = new float[4];
     public GameObject waveObj = null;
 
     public bool active = false;
@@ -26,64 +26,84 @@ public class Spectrum : MonoBehaviour
     void Update()
     {
         GetSpectrumAudioSource();
+        MakeFrequencyBands();
         
         float c1 = samples[3] + samples[2] + samples[4];
         float c2 = samples[11] + samples[12] + samples[13];
         float c3 = samples[22] + samples[23] + samples[24];
         float c4 = samples[44] + samples[45] + samples[46] + samples[47] + samples[48] + samples[49];
-
-        GameObject[] waves = GameObject.FindGameObjectsWithTag("Wave");
-
-        for (int i = 0; i < waves.Length; i++)
-        {
-            switch (waves[i].name)
-            {
-                case "c1":                    
-                    waves[i].transform.localPosition += new Vector3(0, 0, -c1);
-                    break;
-
-                case "c2":                    
-                    waves[i].transform.localPosition += new Vector3(0, 0, -c2);
-                    break;
-
-                case "c3":                    
-                    waves[i].transform.localPosition += new Vector3(0, 0, -c3);
-                    break;
-
-                case "c4":                    
-                    waves[i].transform.localPosition += new Vector3(0, 0, -c4);
-                    break;
-
-            }
-        }
-
-        for (int i = 0; i < samples.Length; i++)
-        {
-            if (samples[i] < 0.035f && !active && timer == 0)
-            {
-                active = true;
-            }
-
-            //if (0.036f < samples[i] < 0.0f && !active && timer == 0)
-
-                if (wave != null && active)
-                    wave.transform.localPosition -= new Vector3(0, 0, c1*0.001f);
-
-            if (wave.transform.localPosition.z < -34.0f)
-            {
-                timer = 0;
-                active = false;
-                wave.transform.localPosition = new Vector3(2f, .0f, 4);
-            }          
-            
-        }
         
+        //GameObject[] waves = GameObject.FindGameObjectsWithTag("Wave");
+
+        //for (int i = 0; i < waves.Length; i++)
+        //{
+        //    switch (waves[i].name)
+        //    {
+        //        case "c1":                    
+        //            waves[i].transform.localPosition += new Vector3(0, 0, -c1);
+        //            break;
+
+        //        case "c2":                    
+        //            waves[i].transform.localPosition += new Vector3(0, 0, -c2);
+        //            break;
+
+        //        case "c3":                    
+        //            waves[i].transform.localPosition += new Vector3(0, 0, -c3);
+        //            break;
+
+        //        case "c4":                    
+        //            waves[i].transform.localPosition += new Vector3(0, 0, -c4);
+        //            break;
+
+        //    }
+        //}
+
+        //for (int i = 0; i < samples.Length; i++)
+        //{
+        //    if (samples[i] < 0.035f && !active && timer == 0)
+        //    {
+        //        active = true;
+        //    }
+
+        //    //if (0.036f < samples[i] < 0.0f && !active && timer == 0)
+
+        //        if (wave != null && active)
+        //            wave.transform.localPosition -= new Vector3(0, 0, c1*0.001f);
+
+        //    if (wave.transform.localPosition.z < -34.0f)
+        //    {
+        //        timer = 0;
+        //        active = false;
+        //        wave.transform.localPosition = new Vector3(2f, .0f, 4);
+        //    }                      
+        //}        
     }
 
     void GetSpectrumAudioSource()
     {
         audio.GetSpectrumData(samples, 0, FFTWindow.Hamming);      
           
+    }
+
+    void MakeFrequencyBands()
+    {
+        int count = 0;
+        for (int i = 0; i < freqBand.Length; i++)
+        {
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+             
+            if(i== 7)
+                sampleCount += 2;
+            
+            for(int j = 0; j < sampleCount; j++)
+            {
+                average += samples[count] * (count + 1);
+                count++;
+            }
+            average /= count;
+            freqBand[i] = average * 10;
+        }
     }
 
 }
